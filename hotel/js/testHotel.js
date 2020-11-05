e0c8784701d56e4d98d9a357fe4749c2077fa859
RaymondJ <396945399@qq.com>
2020-11-05 14:17:58 +0800

    test

---
+++ hotel/js/testHotel.js
@@ -0,0 +1,9 @@
+var testOthersJson = '{"AirMainRids":[2911345],"ApartTime":null,"ApplicationState":null,"ApprovalLevel":0,"ApproveDtailList":[],"ApproveWithoutChoose":false,"BookInOrgOrderForAT":false,"BookTime":null,"CanDownloadBill":false,"Car":[],"ContinueBookIsCreateOrder":false,"DateTimeNowString":null,"ErrorMsg":null,"FileUploadTime":null,"HasTicketTimeOut":false,"HideTrainChange":false,"Hotel":[],"Insurances":[],"MainCompanyId":null,"MoreApproversSubmit":false,"Offline_Pay":false,"OrderCustomer":null,"OrderCustomerDetailList":[],"OrderFare":null,"OrderNo":"202011040345","PaidAmount":null,"PaidVatAmount":null,"PayChannel":null,"Segment":[],"ShowApproval":false,"ShowCancel":false,"ShowDelete":false,"ShowGoOnBook":false,"ShowPayment":false,"ShowPaymentByApprovedA":false,"ShowTicket":false,"StartsOn":null,"State":null,"Sundries":[],"TicketTimeOutMessage":null,"Train":[],"TravelRequestNewMiscellList":[],"UploadFileApprove":false,"Vacation":[],"Visa":[],"YeePayInfoAmount":null,"isCompleted":false}';
+var res = JSON.parse(testOthersJson);
+var isTA = 0
+var orderFinishDetail = new OrderFinishDetail(netUserId.split('\"')[1], res.AirMainRids, orderType, 'CN')
+
+
+function test(){
+    hotelBookOthersPopUp(orderFinishDetail,res.OrderNo); //确认酒店
+}
